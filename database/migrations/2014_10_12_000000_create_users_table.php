c7404ad46b157de046398d437e5d123629da1b1d
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-07-20 15:08:17 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name', 50);
             $table->string('email', 50)->unique();
-            $table->string('password', 50);
+            $table->string('password', 100);
             $table->timestamps();
         });
     }
